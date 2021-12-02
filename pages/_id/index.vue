73c4c243a88384b92750d94f4311c83f047ebb0f
Yohei Kasai <yohei.0119@gmail.com>
2021-12-02 23:39:42 +0900

    first commit

---
+++ pages/_id/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+    <h1>{{ title }}</h1>
+    <p>{{ publishedAt }}</p>
+    <div v-html="body"></div>
+  </main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios, params }) {
+		const data = await $axios.$get(
+      `https://sample0119.microcms.io/api/v1/blog/${params.id}`,
+      {
+        headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+      }
+    );
+    return data
+	}
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <ul>
+    <li v-for="content in contents" :key="content.id">
+      <nuxt-link :to="`/${content.id}`">
+        {{ content.title }}
+      </nuxt-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  async asyncData({ $axios }) {
+		const data = await $axios.$get(
+      'https://sample0119.microcms.io/api/v1/blog',
+      {
+        headers: { 'X-MICROCMS-API-KEY': process.env.API_KEY }
+      }
+    );
+    return data
+	}
+}
+</script>
